33565c1dd3aaeeacbc6a85f4aea97f1df3b3fb7f
Kemal Kayahan <kemal@mynextmatch.com>
2019-07-28 19:06:10 +0300

    master Created BaseEntity class for entities in order to be able to inherit from

---
+++ src/main/java/com/camelkyhn/springtodolistapi/middleware/bases/BaseEntity.java
@@ -0,0 +1,70 @@
+package com.camelkyhn.springtodolistapi.middleware.bases;
+
+import com.camelkyhn.springtodolistapi.middleware.enums.Status;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.util.Date;
+
+@MappedSuperclass
+public abstract class BaseEntity
+{
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+
+    private Date createdDate;
+
+    private Date updatedDate;
+
+    private Status status;
+
+    public BaseEntity()
+    {
+        this.createdDate = new Date();
+        this.updatedDate = new Date();
+        this.status = Status.Active;
+    }
+
+    public Long getId()
+    {
+        return id;
+    }
+
+    public void setId(Long id)
+    {
+        this.id = id;
+    }
+
+    public Date getCreatedDate()
+    {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate)
+    {
+        this.createdDate = createdDate;
+    }
+
+    public Date getUpdatedDate()
+    {
+        return updatedDate;
+    }
+
+    public void setUpdatedDate(Date updatedDate)
+    {
+        this.updatedDate = updatedDate;
+    }
+
+    public Status getStatus()
+    {
+        return status;
+    }
+
+    public void setStatus(Status status)
+    {
+        this.status = status;
+    }
+}
