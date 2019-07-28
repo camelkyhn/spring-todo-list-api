b92300f2612d64e6aa2ecc6b4028ba2b00d94286
Kemal Kayahan <kemal@mynextmatch.com>
2019-07-29 00:08:29 +0300

    master Created Role class.

---
+++ src/main/java/com/camelkyhn/springtodolistapi/middleware/entities/Role.java
@@ -0,0 +1,33 @@
+package com.camelkyhn.springtodolistapi.middleware.entities;
+
+import com.camelkyhn.springtodolistapi.middleware.bases.BaseEntity;
+import org.hibernate.validator.constraints.Length;
+
+import javax.persistence.Entity;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+public class Role extends BaseEntity
+{
+    @Length(max=20, min=3)
+    @NotEmpty
+    private String name;
+
+    public Role() { super(); }
+
+    public Role(@Length(max = 20, min = 3) @NotEmpty String name)
+    {
+        super();
+        this.name = name;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+}
