422881428237b1df45797cd18700ba4d714a519a
Kemal Kayahan <kemal@mynextmatch.com>
2019-07-28 19:00:34 +0300

    master Created project with "Spring Initializer".

---
+++ src/main/java/com/camelkyhn/springtodolistapi/SpringTodoListApiApplication.java
@@ -0,0 +1,13 @@
+package com.camelkyhn.springtodolistapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTodoListApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTodoListApiApplication.class, args);
+	}
+
+}
