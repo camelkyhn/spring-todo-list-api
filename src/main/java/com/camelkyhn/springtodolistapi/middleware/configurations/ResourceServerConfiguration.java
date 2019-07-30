d7adc8f134d1d1fcb5e181490982a71a3e199422
Kemal Kayahan <kemal@mynextmatch.com>
2019-07-30 03:05:43 +0300

    master Created ResourceServerConfigurations class for permissions.

---
+++ src/main/java/com/camelkyhn/springtodolistapi/middleware/configurations/ResourceServerConfiguration.java
@@ -0,0 +1,22 @@
+package com.camelkyhn.springtodolistapi.middleware.configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter
+{
+    @Override
+    public void configure(HttpSecurity http) throws Exception
+    {
+        http.authorizeRequests()
+                .antMatchers("/h2-console/**").permitAll()
+                .antMatchers("/User/Create").permitAll()
+                .antMatchers("/oauth/token").permitAll()
+                .anyRequest().authenticated();
+        http.headers().frameOptions().disable();
+    }
+}
