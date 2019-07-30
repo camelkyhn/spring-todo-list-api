eeff0e41c439d4256b0a64aa7896819e76f28a7d
Kemal Kayahan <kemal@mynextmatch.com>
2019-07-31 02:12:25 +0300

    master Prettier code.

--- src/main/java/com/camelkyhn/springtodolistapi/middleware/bases/IService.java
+++ src/main/java/com/camelkyhn/springtodolistapi/middleware/bases/IService.java
@@ -2,11 +2,14 @@ package com.camelkyhn.springtodolistapi.middleware.bases;
 
 import java.util.List;
 
-public interface IService<T extends BaseEntity, TDto extends BaseDto, TFilterDto extends BaseFilterDto>
-{
+public interface IService<T extends BaseEntity, TDto extends BaseDto, TFilterDto extends BaseFilterDto> {
     Result<T> get(Long id);
+
     Result<List<T>> list(TFilterDto filterDto);
+
     Result<T> create(TDto dto);
+
     Result<T> update(Long id, TDto dto);
+
     Result<Boolean> delete(Long id);
 }
