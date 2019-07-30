3ed093041ac591f4585dc504532439e8a9b365b5
Kemal Kayahan <kemal@mynextmatch.com>
2019-07-30 17:07:00 +0300

    master Added JpaSpecificationExecutor functions to repository interface.

--- src/main/java/com/camelkyhn/springtodolistapi/service/role/IRoleRepository.java
+++ src/main/java/com/camelkyhn/springtodolistapi/service/role/IRoleRepository.java
@@ -2,6 +2,7 @@ package com.camelkyhn.springtodolistapi.service.role;
 
 import com.camelkyhn.springtodolistapi.middleware.entities.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface IRoleRepository extends JpaRepository<Role, Long> {
+public interface IRoleRepository extends JpaSpecificationExecutor<Role>, JpaRepository<Role, Long> {
 }
