367197f8d1543706fd12ab6e4f8d9b06221856a4
Kemal Kayahan <kemal@mynextmatch.com>
2019-07-30 19:19:20 +0300

    master Added JpaSpecificationExecutor functions.

--- src/main/java/com/camelkyhn/springtodolistapi/service/todo/ITodoRepository.java
+++ src/main/java/com/camelkyhn/springtodolistapi/service/todo/ITodoRepository.java
@@ -2,6 +2,7 @@ package com.camelkyhn.springtodolistapi.service.todo;
 
 import com.camelkyhn.springtodolistapi.middleware.entities.Todo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface ITodoRepository extends JpaRepository<Todo, Long> {
+public interface ITodoRepository extends JpaRepository<Todo, Long>, JpaSpecificationExecutor<Todo> {
 }
--- src/main/java/com/camelkyhn/springtodolistapi/service/todolist/ITodoListRepository.java
+++ src/main/java/com/camelkyhn/springtodolistapi/service/todolist/ITodoListRepository.java
@@ -2,6 +2,7 @@ package com.camelkyhn.springtodolistapi.service.todolist;
 
 import com.camelkyhn.springtodolistapi.middleware.entities.TodoList;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface ITodoListRepository extends JpaRepository<TodoList, Long> {
+public interface ITodoListRepository extends JpaRepository<TodoList, Long>, JpaSpecificationExecutor<TodoList> {
 }
