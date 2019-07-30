4d457e64ca242ad44f99f53301770eb2667083b7
Kemal Kayahan <kemal@mynextmatch.com>
2019-07-30 19:35:16 +0300

    master Removed id.

--- src/main/java/com/camelkyhn/springtodolistapi/middleware/bases/BaseDto.java
+++ src/main/java/com/camelkyhn/springtodolistapi/middleware/bases/BaseDto.java
@@ -4,19 +4,9 @@ import com.camelkyhn.springtodolistapi.middleware.enums.Status;
 
 public abstract class BaseDto {
 
-    private Long id;
-
     private Status status;
 
     public BaseDto() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
     }
 
     public Status getStatus() {
