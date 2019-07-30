45e8f78573c5119dbb31e4f8ad0eefe34415af6d
Kemal Kayahan <kemal@mynextmatch.com>
2019-07-31 02:12:02 +0300

    master Added bindingResult parameter to be able to check the submitted model.

--- src/main/java/com/camelkyhn/springtodolistapi/middleware/bases/IController.java
+++ src/main/java/com/camelkyhn/springtodolistapi/middleware/bases/IController.java
@@ -1,12 +1,17 @@
 package com.camelkyhn.springtodolistapi.middleware.bases;
 
+import org.springframework.validation.BindingResult;
+
 import java.util.List;
 
-public interface IController<T extends BaseEntity, TDto extends BaseDto, TFilterDto extends BaseFilterDto>
-{
+public interface IController<T extends BaseEntity, TDto extends BaseDto, TFilterDto extends BaseFilterDto> {
     Result<T> get(Long id);
+
     Result<List<T>> list(TFilterDto filterDto);
-    Result<T> create(TDto dto);
-    Result<T> update(Long id, TDto dto);
+
+    Result<T> create(TDto dto, BindingResult bindingResult);
+
+    Result<T> update(Long id, TDto dto, BindingResult bindingResult);
+
     Result<Boolean> delete(Long id);
 }
