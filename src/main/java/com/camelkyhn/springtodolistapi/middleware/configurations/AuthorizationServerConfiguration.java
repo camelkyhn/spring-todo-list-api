f6921c419accd719bf872744ea339e4201b558d9
Kemal Kayahan <kemal@mynextmatch.com>
2019-07-30 03:07:08 +0300

    master Created AuthorizationServerConfiguration class for authorization configurations.

---
+++ src/main/java/com/camelkyhn/springtodolistapi/middleware/configurations/AuthorizationServerConfiguration.java
@@ -0,0 +1,47 @@
+package com.camelkyhn.springtodolistapi.middleware.configurations;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfiguration extends AuthorizationServerConfigurerAdapter
+{
+    private final AuthenticationManager authenticationManager;
+
+    @Autowired
+    public AuthorizationServerConfiguration(AuthenticationManager authenticationManager)
+    {
+        this.authenticationManager = authenticationManager;
+    }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security)
+    {
+        security.checkTokenAccess("isAuthenticated()");
+    }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception
+    {
+        clients.inMemory().withClient("my-trusted-client")
+                .authorizedGrantTypes("client_credentials", "password", "refresh_token")
+                .authorities("ROLE_CLIENT", "ROLE_TRUSTED_CLIENT")
+                .scopes("read", "write", "trust")
+                .resourceIds("oauth2-resource")
+                .accessTokenValiditySeconds(60 * 60 * 2) // 2 hours
+                .secret("{noop}secret");
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints)
+    {
+        endpoints.authenticationManager(authenticationManager);
+    }
+}
