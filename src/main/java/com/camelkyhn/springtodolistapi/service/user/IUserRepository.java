2558c0ac9b2e3771dc604ef27ea91792f862b6b9
Kemal Kayahan <kemal@mynextmatch.com>
2019-07-30 19:15:12 +0300

    master Added JpaSpecificationExecutor functions.

--- src/main/java/com/camelkyhn/springtodolistapi/service/user/IUserRepository.java
+++ src/main/java/com/camelkyhn/springtodolistapi/service/user/IUserRepository.java
@@ -2,11 +2,11 @@ package com.camelkyhn.springtodolistapi.service.user;
 
 import com.camelkyhn.springtodolistapi.middleware.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-public interface IUserRepository extends JpaRepository<User, Long>
-{
+public interface IUserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
     @Query("select u from User u where u.username = :username and not (u.status = 2)")
     User findByUsername(@Param("username") String username);
 }
