dc1249ca94546dd5165f4e601b48293a095317df
Kemal Kayahan <kemal@mynextmatch.com>
2019-07-31 14:37:24 +0300

    master Overloaded constructor.

--- src/main/java/com/camelkyhn/springtodolistapi/middleware/exceptions/InvalidModelStateException.java
+++ src/main/java/com/camelkyhn/springtodolistapi/middleware/exceptions/InvalidModelStateException.java
@@ -1,7 +1,15 @@
 package com.camelkyhn.springtodolistapi.middleware.exceptions;
 
+import org.springframework.validation.ObjectError;
+
+import java.util.Objects;
+
 public class InvalidModelStateException extends Exception {
     public InvalidModelStateException(String model) {
         super("This " + model + " model is null/empty or not in expected format!");
+    }
+
+    public InvalidModelStateException(ObjectError error) {
+        super("Field error in object: [" + error.getObjectName() + "], code: [" + Objects.requireNonNull(error.getCodes())[0] + "], message: [" + error.getDefaultMessage() + "]");
     }
 }
