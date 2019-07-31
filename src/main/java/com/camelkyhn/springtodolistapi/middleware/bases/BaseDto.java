385c78e19990f39bd182f2e5804366e7701265b0
Kemal Kayahan <kemal@mynextmatch.com>
2019-07-31 14:50:54 +0300

    master Added not null annotation.

--- src/main/java/com/camelkyhn/springtodolistapi/middleware/bases/BaseDto.java
+++ src/main/java/com/camelkyhn/springtodolistapi/middleware/bases/BaseDto.java
@@ -2,8 +2,11 @@ package com.camelkyhn.springtodolistapi.middleware.bases;
 
 import com.camelkyhn.springtodolistapi.middleware.enums.Status;
 
+import javax.validation.constraints.NotNull;
+
 public abstract class BaseDto {
 
+    @NotNull
     private Status status;
 
     public BaseDto() {
