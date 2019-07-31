ce0c16a057b1a3e376eed2e8c965ec33460214d9
Kemal Kayahan <kemal@mynextmatch.com>
2019-07-31 17:07:03 +0300

    master Added applier method for base filters.

--- src/main/java/com/camelkyhn/springtodolistapi/middleware/bases/BaseService.java
+++ src/main/java/com/camelkyhn/springtodolistapi/middleware/bases/BaseService.java
@@ -4,9 +4,22 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import java.util.ArrayList;
 import java.util.List;
 
 public class BaseService<TRepository extends JpaSpecificationExecutor<TEntity>, TEntity> {
+    protected List<Predicate> applyBaseFilters(BaseFilterDto filterDto, CriteriaBuilder criteriaBuilder, Root<TEntity> root) {
+        List<Predicate> predicates = new ArrayList<>();
+        if (filterDto.getStatus() != null) {
+            predicates.add(criteriaBuilder.and(criteriaBuilder.equal(root.get("status"), filterDto.getStatus())));
+        }
+
+        return predicates;
+    }
+
     protected List<TEntity> applyPagination(TRepository repository, BaseFilterDto filterDto, Specification<TEntity> specification) {
         List<TEntity> resultList;
         filterDto.setTotalCount(repository.count(specification));
